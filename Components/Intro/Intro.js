f72d23e147d69ae25a9e9429b3bc9b7f4f34fd55
Raphael Donanu <raphaeldonanu@Administrators-MacBook-Pro-8.local>
2021-12-01 11:38:49 +0100

    fixing styles

--- Components/Intro/Intro.js
+++ Components/Intro/Intro.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
 import profilePic from '../../public/img/IMG_20211118_150540.jpg';
@@ -26,42 +26,44 @@ const Intro = ({ toggleTheme, theme, componentMounted }) => {
     };
 
     return (
-        <section className={styles.containerx} id="index">
-            <div className={styles.toggleHolder}>
-                <Toggle theme={theme} toggleTheme={toggleTheme} />
-            </div>
-            <div className={styles.intro}>
-                <div className={composeClasses(styles.imageholder, styles.animate, styles.pop, styles.delay12)}>
-                    <Image
-                        src={profilePic}
-                        alt="Picture of the author"
-                        width={350}
-                        height={350}
-                        className={styles.image}
-                    />
+        <Fragment>
+            <section className={styles.containerx} id="index">
+                <div className={styles.toggleHolder}>
+                    <Toggle theme={theme} toggleTheme={toggleTheme} />
                 </div>
+                <div className={styles.intro}>
+                    <div className={composeClasses(styles.imageholder, styles.animate, styles.pop, styles.delay12)}>
+                        <Image
+                            src={profilePic}
+                            alt="Picture of the author"
+                            width={350}
+                            height={350}
+                            className={styles.image}
+                        />
+                    </div>
 
-                <div className={composeClasses(styles.text, styles.animate, styles.flip, styles.delay2)}>
+                    <div className={composeClasses(styles.text, styles.animate, styles.flip, styles.delay2)}>
 
-                    <h1>Hello, I'm <span>Raphael Donanu
-                    </span><br />I am a front-end engineer, <br /> based in Lagos, Nigeria </h1>
+                        <h1>Hello, I'm <span>Raphael Donanu
+                        </span><br />I am a front-end engineer, <br /> based in Lagos, Nigeria </h1>
+
+                    </div>
+
 
                 </div>
-
-
-            </div>
-            <div className={styles.social}>
-                <a href="https://linkedin.com/in/raphael-donanu" target="_blank" rel="noopener noreferrer"><AiFillLinkedin style={style} /></a>
-                <a href="https://github.com/Raphael2k6" target="_blank" rel="noopener noreferrer"><AiFillGithub style={style} /></a>
-                <a href="https://www.facebook.com/raphael.donanu" target="_blank" rel="noopener noreferrer"><AiFillFacebook style={style} /></a>
-                <a href="https://www.instagram.com/tonubarii/?hl=en" target="_blank" rel="noopener noreferrer"> <AiFillInstagram style={style} /></a>
-            </div>
-            <div className={composeClasses(styles.more, styles.animate, styles.slide, styles.delay3)}>
-                <a href="#about">
-                    <button>More <AiOutlineArrowDown /></button>
-                </a>
-            </div>
-        </section>
+                <div className={styles.social}>
+                    <a href="https://linkedin.com/in/raphael-donanu" target="_blank" rel="noopener noreferrer"><AiFillLinkedin style={style} /></a>
+                    <a href="https://github.com/Raphael2k6" target="_blank" rel="noopener noreferrer"><AiFillGithub style={style} /></a>
+                    <a href="https://www.facebook.com/raphael.donanu" target="_blank" rel="noopener noreferrer"><AiFillFacebook style={style} /></a>
+                    <a href="https://www.instagram.com/tonubarii/?hl=en" target="_blank" rel="noopener noreferrer"> <AiFillInstagram style={style} /></a>
+                </div>
+                <div className={composeClasses(styles.more, styles.animate, styles.slide, styles.delay3)}>
+                    <a href="#about">
+                        <button>More <AiOutlineArrowDown /></button>
+                    </a>
+                </div>
+            </section>
+        </Fragment>
     )
 }
 
